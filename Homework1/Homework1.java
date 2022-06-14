3fae59c06172ac2b4f5c1400b1b9f8f0c8cca3b7
Alexander Vetrenko <vetrenko.ag@gmail.com>
2022-06-14 20:02:26 +0500

    Homework 1

---
+++ Homework1/Homework1.java
@@ -0,0 +1,23 @@
+//Написать программу вычисления n-ого треугольного числа
+
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Введите порядковый номер треугольного числа: \n");
+        String s = sc.nextLine();
+        try{
+            int nm = Integer.parseInt(s.trim());
+            System.out.printf("Треугольное число с порядковым номером %d равно %d \n", nm, Triangle_Nmb(nm));
+        }
+        catch (NumberFormatException nfe){
+            System.out.println("Введите корректное число: " + nfe.getMessage());
+        }
+        sc.close();
+    }
+
+    public static int Triangle_Nmb(int m){
+        return (int) (0.5*m*(m+1));
+    } 
+}
