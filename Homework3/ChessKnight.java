35f5946e3f3b12849b9e985d1b139f391086efbd
Alexander Vetrenko <vetrenko.ag@gmail.com>
2022-07-02 18:35:12 +0500

    02.07.2022. Ушатал коня!Это лучше, чем он - меня!

--- Homework3/ChessKnight.java
+++ Homework3/ChessKnight.java
@@ -2,71 +2,96 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+//Реализовал правило Вансдорфа. Работает не всегда стабильно.
+//Поэтому, если вылетела ошибка, надо перезапустить.
+//Скорее всего это связано с тем, что конь может зайти в тупик, если начинает с определенных ячеек.
+//Как вариант, можно начальное положение коня было бы ограничить углами или серединой доски.
+//Но делать этого не стал, поскольку и вариант с вылетом программы тоже представляет интерес.
+//Программу ограничил интервалом ширины доски 5-11 клеток.
+//В принципе, она работает и для 100 клеток - пробовал - но долго, минуты 3 у меня машина считала, и посчитала.
 
 public class ChessKnight {
     public static void main(String[] args) {
-        int n = Razmer_Doski(); 
-        //System.out.println(n);
-        //System.out.println(Nomer_Polya(2, 7, n));
-        //Print_Array(Koordinaty_Polya(64, n));
-        //Narisovat_Dosku(n);
         List<List<Integer>> Log = new ArrayList<>();
+        List<List<Integer>> Prom_List = new ArrayList<>();
+        int n = Razmer_Doski(); 
         Log = Perviy_Chod(n);
-        System.out.println(Log);
-        System.out.println(Poisk_Choda(Log.get(Log.size()-1), n));
+        while(Log.size()<n*n){
+            Prom_List = Poisk_Choda(Log.get(Log.size()-1), n, Log);
+            int min; int index=0;
+            min = Poisk_Choda(Prom_List.get(0),n,Log).size();
+            for (List<Integer> item : Prom_List){
+                if(Poisk_Choda(item,n,Log).size()<min) {
+                    min = Poisk_Choda(item,n,Log).size();
+                    index = Prom_List.indexOf(item);
+                }
+            }
+            Log.add(Prom_List.get(index));
+        }
+        Narisovat_Dosku(n);
+        System.out.println();
+        for (List<Integer> item : Log){
+            System.out.printf("%d -> ",Nomer_Polya(item, n));
+        }
     }
     
-    public static List<List<Integer>> Poisk_Choda(List<Integer> list, int m){
+    public static List<List<Integer>> Poisk_Choda(List<Integer> list, int m, List<List<Integer>> big_list){
+        List<List<Integer>> res1 = new ArrayList<>();
         List<List<Integer>> res = new ArrayList<>();
-        if((list.get(0)-2)>0 && (list.get(1)-1)>0){
+        
+        if((list.get(0)-2)>=0 && (list.get(1)-1)>=0){
             List<Integer> temp = new ArrayList<>();
             temp.add(list.get(0)-2);
             temp.add(list.get(1)-1);
-            res.add(temp);
+            res1.add(temp);
         }
-        if((list.get(0)-2)>0 && (list.get(1)+1)<m){
+        if((list.get(0)-2)>=0 && (list.get(1)+1)<m){
             List<Integer> temp = new ArrayList<>();
             temp.add(list.get(0)-2);
             temp.add(list.get(1)+1);
-            res.add(temp);
+            res1.add(temp);
         }
-        if((list.get(0)-1)>0 && (list.get(1)-2)>0){
+        if((list.get(0)-1)>=0 && (list.get(1)-2)>=0){
             List<Integer> temp = new ArrayList<>();
             temp.add(list.get(0)-1);
             temp.add(list.get(1)-2);
-            res.add(temp);
+            res1.add(temp);
         }
-        if((list.get(0)-1)>0 && (list.get(1)+2)<m){
+        if((list.get(0)-1)>=0 && (list.get(1)+2)<m){
             List<Integer> temp = new ArrayList<>();
             temp.add(list.get(0)-1);
             temp.add(list.get(1)+2);
-            res.add(temp);
+            res1.add(temp);
         }
-        if((list.get(0)+1)<m && (list.get(1)-2)>0){
+        if((list.get(0)+1)<m && (list.get(1)-2)>=0){
             List<Integer> temp = new ArrayList<>();
             temp.add(list.get(0)+1);
             temp.add(list.get(1)-2);
-            res.add(temp);
+            res1.add(temp);
         }
         if((list.get(0)+1)<m && (list.get(1)+2)<m){
             List<Integer> temp = new ArrayList<>();
             temp.add(list.get(0)+1);
             temp.add(list.get(1)+2);
-            res.add(temp);
+            res1.add(temp);
         }
-        if((list.get(0)+2)<m && (list.get(1)-1)>0){
+        if((list.get(0)+2)<m && (list.get(1)-1)>=0){
             List<Integer> temp = new ArrayList<>();
             temp.add(list.get(0)+2);
             temp.add(list.get(1)-1);
-            res.add(temp);
+            res1.add(temp);
         }
         if((list.get(0)+2)<m && (list.get(1)+1)<m){
             List<Integer> temp = new ArrayList<>();
             temp.add(list.get(0)+2);
             temp.add(list.get(1)+1);
-            res.add(temp);
+            res1.add(temp);
         }
-        
+        for (List<Integer> item : res1) {
+            if(big_list.contains(item)==false){
+                res.add(item);
+            }
+        } 
         return res;
     }
     public static List<List<Integer>> Perviy_Chod(int m){
@@ -89,7 +114,10 @@ public class ChessKnight {
         for (int i = 0; i < arr.length; i++) {
             
             for (int j = 0; j < arr[0].length; j++) {
-                System.out.printf("| %-3d",Nomer_Polya(i, j, m));
+                List<Integer> temp = new ArrayList<>();
+                temp.add(i);
+                temp.add(j);
+                System.out.printf("| %-3d",Nomer_Polya(temp, m));
             }
             System.out.print("|");
             System.out.println();
@@ -97,31 +125,19 @@ public class ChessKnight {
             System.out.println();
         }
     }
-    public static void Print_Array(int [] arr){
-        for (int i = 0; i < arr.length; i++) {
-            System.out.printf(arr[i] + " ");
-        }
-    }
-    public static int[] Koordinaty_Polya(int n, int s){
-        int [] arr = new int[2];
-        if(n%s == 0) arr[0] = n/s-1;
-        else arr[0] = n/s;
-        arr[1] = n-(arr[0]*s)-1;
-        return arr;
-    }
-    public static int Nomer_Polya(int i,int j, int s){   
-        return i*s+j+1;
+    public static int Nomer_Polya(List<Integer> list, int m){   
+        return list.get(0)*m+list.get(1)+1;
     }
     public static int Razmer_Doski(){     
         int nm = Proverka_Chisla();
-        while(nm<5){
+        while(nm<5 || nm>11){
             nm=Proverka_Chisla();
         }
         return nm;
     }
     public static int Proverka_Chisla(){
         Scanner sc = new Scanner(System.in);//ругается на утечку данных из-за незакрываемого сканнера.
-        System.out.printf("Введите сторону шахматной доски (целые числа не менее 5): \n");
+        System.out.printf("Введите сторону шахматной доски (целые числа не менее 5 и не более 11): \n");
         String s = sc.nextLine();
         int nmb;
         try{
