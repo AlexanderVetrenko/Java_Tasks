052d8a3cd0f24c12eb17194e8e21586a05852b87
Alexander Vetrenko <vetrenko.ag@gmail.com>
2022-07-04 20:53:25 +0500

    04.07.2022

--- Homework3/ChessKnight.java
+++ Homework3/ChessKnight.java
@@ -1,3 +1,5 @@
+package Homework3;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
---
+++ Homework4/postfix.java
@@ -0,0 +1,29 @@
+package Homework4;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+public class postfix {
+    public static void main(String[] args) {
+        List<Character> arif_dos = List.of('+','-','*','/','^','(',')');
+        //System.out.println(arif_dos);
+        String s = "5*6+(2-9)";
+        char [] arr = s.toCharArray();
+        Queue<Character> chisla = new LinkedList<Character>();
+        Stack<Character> operatory = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+              if(Character.isDigit(arr[i])){
+                chisla.add(arr[i]);
+              }
+              if(arif_dos.contains(arr[i])){
+                if(operatory.isEmpty() || operatory.peek()=='('){
+                    operatory.push(arr[i]);
+                }
+              }
+        }
+
+
+    }
+}
